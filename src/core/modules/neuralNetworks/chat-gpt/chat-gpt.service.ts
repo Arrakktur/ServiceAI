00078f68d09d23a12ff90b4d5a6f4cb194d2a324
e.ischenko <Crydev.egor>
2023-03-28 23:33:13 +0300

    init service

--- src/core/modules/neuralNetworks/chat-gpt/chat-gpt.service.ts
+++ src/core/modules/neuralNetworks/chat-gpt/chat-gpt.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { Configuration, OpenAIApi } from "openai";
 import { OPENAI_API_KEY } from "../../../models/constants/tokens";
 
@@ -6,20 +6,31 @@ import { OPENAI_API_KEY } from "../../../models/constants/tokens";
 export class ChatGptService {
   private readonly configuration;
   private openai: OpenAIApi;
+  private context = [];
 
-
-  constructor(){
+  constructor() {
     this.configuration = new Configuration({
-      apiKey: OPENAI_API_KEY,
+      apiKey: OPENAI_API_KEY
     });
     this.openai = new OpenAIApi(this.configuration);
   }
 
   async chat(message: string): Promise<string> {
+    this.updateContext({ "role": "user", "content": message });
     const completion = await this.openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [{'role': 'user', 'content': message}],
+      messages: this.context,
+      temperature: 0.5
     });
-    return completion.data.choices[0].message.content;
+    const response = completion.data.choices[0].message.content;
+    this.updateContext({ "role": "assistant", "content": response });
+    return response;
+  }
+
+  updateContext(object: { "role": string, "content": string }) {
+    this.context.push(object);
+    if (this.context.length > 10) {
+      this.context.splice(0, 1);
+    }
   }
 }
