6d9880e557cf33d14f3edbce9a870917ae0d09a1
e.ischenko <Crydev.egor>
2023-03-29 14:32:51 +0300

    init service

--- src/core/modules/bots/discord/services/discord.service.ts
+++ src/core/modules/bots/discord/services/discord.service.ts
@@ -21,44 +21,48 @@ export class DiscordService {
       ]});
 
     client.on("messageCreate", async (message) => {
-      if (!this.allowChannel.includes(message.channelId)) return;
-      if (message.author.bot) return;
-      const command = message.content
+      try {
+        if (!this.allowChannel.includes(message.channelId)) return;
+        if (message.author.bot) return;
+        const command = message.content
 
-      if (command === "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`{system} ping: ${timeTaken}ms.`);
-        return;
-      }
-
-      if (command === "help") {
-        message.reply('{system} Список доступных команд:\n' +
-          'help - список команд\n' +
-          'getContext - получить текущий контекст\n' +
-          'clearContext - удалить текущий контекст' +
-          '\n\n' +
-          'Системные сообщения не относящиеся к сообщениям бота помечаются {system} в начале сообщения')
-       return;
-      }
-
-      if(command === "getContext") {
-        if (this.chatGptService.contextLength){
-          message.reply(`{system} Контекст бота (${this.chatGptService.contextLength} сообщений)\n` + this.chatGptService.context);
-        } else {
-          message.reply(`{system} Контекст пуст`)
+        if (command === "ping") {
+          const timeTaken = Date.now() - message.createdTimestamp;
+          message.reply(`{system} ping: ${timeTaken}ms.`);
+          return;
         }
-        return;
-      }
 
-      if(command === 'clearContext') {
-        this.chatGptService.clearContext();
-        message.reply('{system}: Контекст очищен')
-        return;
-      }
+        if (command === "help") {
+          message.reply('{system} Список доступных команд:\n' +
+            'help - список команд\n' +
+            'getContext - получить текущий контекст\n' +
+            'clearContext - удалить текущий контекст' +
+            '\n\n' +
+            'Системные сообщения не относящиеся к сообщениям бота помечаются {system} в начале сообщения')
+          return;
+        }
 
-      // ответ gpt
-      const answer = await this.chatGptService.chat(command);
-      message.reply(answer);
+        if (command === "getContext") {
+          if (this.chatGptService.contextLength) {
+            message.reply(`{system} Контекст бота (${this.chatGptService.contextLength} сообщений)\n` + this.chatGptService.context);
+          } else {
+            message.reply(`{system} Контекст пуст`)
+          }
+          return;
+        }
+
+        if (command === 'clearContext') {
+          this.chatGptService.clearContext();
+          message.reply('{system}: Контекст очищен')
+          return;
+        }
+
+        // ответ gpt
+        const answer = await this.chatGptService.chat(command);
+        message.reply(answer);
+      } catch (error) {
+        message.reply(`{system} Ошибка выполнения\n${error}`);
+      }
     })
 
     client.login(DISCORD_BOT_TOKEN);
